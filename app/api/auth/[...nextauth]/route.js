168f1c33ac226ab8dffebf27458f17ed886e5cce
Xavier <manikandan.a@analyticbrains.com>
2025-02-04 13:17:39 +0530

    Test commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,6 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
-
 const handler = NextAuth({
   providers: [
     GoogleProvider({
